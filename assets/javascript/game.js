704925bda05b073e66e65142149bd2cc20b9bb53
Brent <mina512@live.com>
2019-09-29 18:15:12 -0500

    updated js to display scores

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,7 +10,6 @@ function ranNum() {
 }
 
 function crystalNumber() {
-    $("img").attr("data-crysalvalue");
     for (var i = 0; i < 4; i++) {
         var tempNum = Math.floor((Math.random()*8)+1);
         crystalValue[i] = tempNum;
@@ -18,6 +17,24 @@ function crystalNumber() {
     $("#emerald").attr("data-crystalvalue", crystalValue[0]);
     $("#ruby").attr("data-crystalvalue", crystalValue[1]);
     $("#sapphire").attr("data-crystalvalue", crystalValue[2]);
-    $("#amethyst").attr(data-crystalvalue, crystalValue[3]);
+    $("#amethyst").attr("data-crystalvalue", crystalValue[3]);
 }
 
+crystalNumber();
+ranNum();
+console.log(crystalValue);
+
+$(".gems").on("click", function() {
+    var gemValue = ($(this).attr("data-crystalvalue"));
+    gemValue = parseInt(gemValue);
+    totalScore += gemValue;
+    $("#total-score").text(totalScore);
+    if (totalScore === randomNumber) {
+        wins++;
+        $("#number-wins").text(wins);
+    }
+    else if (totalScore > randomNumber) {
+        loss++;
+        $("#number-loss").text(loss);
+    }
+})
